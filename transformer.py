eb2eb0b9b73b42e1cbe83b6d4dc5f4e2a69d6063
Vineet Kosaraju <vineetsk1@gmail.com>
2019-03-04 18:55:32 -0800

    add DataSet loader

---
+++ transformer.py
@@ -0,0 +1,117 @@
+import numpy as numpy
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math, copy, time
+from torch.autograd import Variable
+
+import matplotlib.pyplot as plt
+import seaborn
+
+from torchtext import data, datasets
+import spacy
+
+from dataset import CustomDataset
+
+#====================================================================
+
+global max_src_in_batch
+global max_tgt_in_batch
+
+spacy_en = spacy.load('en')
+
+BOS_WORD = '<s>'
+EOS_WORD = '</s>'
+BLANK_WORD = '<blank>'
+SRC = data.Field(tokenize=tokenize_en, pad_token=BLANK_WORD)
+TGT = data.Field(tokenize=tokenize_en, init_token=BOS_WORD, eos_token=EOS_WORD, pad_token=BLANK_WORD)
+
+# CREATE train, val, text
+train, val, test = CustomDataset.splits(
+	fields=(SRC, TGT))
+MIN_FREQ = 2
+
+SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+
+BATCH_SIZE = 12000
+train_iter = MyIterator(train, batch_size=BATCH_SIZE, device=0,
+                        repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                        batch_size_fn=batch_size_fn, train=True)
+valid_iter = MyIterator(val, batch_size=BATCH_SIZE, device=0,
+                        repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                        batch_size_fn=batch_size_fn, train=False)
+test_iter = MyIterator(test, batch_size=BATCH_SIZE, device=0,
+                        repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                        batch_size_fn=batch_size_fn, train=False)
+
+
+#====================================================================
+
+class MyIterator(data.Iterator):
+    def create_batches(self):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn)
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+            self.batches = pool(self.data(), self.random_shuffler)
+            
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size,
+                                          self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
+
+def rebatch(pad_idx, batch):
+    "Fix order in torchtext to match ours"
+    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
+    return Batch(src, trg, pad_idx)
+
+def tokenize_en(text):
+	return [tok.text for tok in spacy_en.tokenizer(text)]
+
+def subsequent_mask(size):
+	attn_shape = (1, size, size)
+	subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+	return torch.from_numpy(subsequent_mask) == 0
+
+class Batch:
+	def __init__(self, src, trg=None, pad=0):
+		self.src = src
+		self.src_mask = (src != pad).unsqueeze(-2)
+		if trg is not None:
+			self.trg = trg[:, :-1]
+			self.trg_y = trg[:, 1:]
+			self.trg_mask = self.make_std_mask(self.trg, pad)
+			self.ntokens = (self.trg_y != pad).data.sum()
+
+	@staticmethod
+	def make_std_mask(tgt, pad):
+		tgt_mask = (tgt != pad).unsqueeze(-2)
+		tgt_mask = tgt_mask & Variable(subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+		return tgt_mask
+
+def batch_size_fn(new, count, sofar):
+	global max_src_in_batch
+	global max_tgt_in_batch
+	if count == 1:
+		max_src_in_batch = 0
+		max_tgt_in_batch = 0
+	max_src_in_batch = max(max_src_in_batch, len(new.src))
+	max_tgt_in_batch = max(max_tgt_in_batch, len(new.trg) + 2)
+	src_elements = count * max_src_in_batch
+	tgt_elements = count * max_tgt_in_batch
+	return max(src_elements, tgt_elements)
+
+
+for i, batch in enumerate(valid_iter):
+	print(i)
+	print(batch.src)
+	print(batch.trg)
+	print(batch.src_mask)
+	print(batch.trg_mask)
+	raise "Test"
